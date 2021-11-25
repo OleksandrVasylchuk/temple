1f8ca9b6e5f8dd3f51b58585f3b31bde84ab2448
Саша Васильчук <sanyabatyushka@gmail.com>
2021-11-25 20:25:58 +0200

    Зробив слайдер

--- index.js
+++ index.js
@@ -13,3 +13,17 @@
     refs.menu.classList.toggle("is-hidden");
   }
 })();
+
+var slideshow = new TINY.slider.slide("slideshow", {
+  id: "slider",
+  auto: 4,
+  resume: false,
+  vertical: false,
+  navid: "pagination",
+  activeclass: "current",
+  position: 0,
+  rewind: false,
+  elastic: true,
+  left: "slideleft",
+  right: "slideright",
+});
